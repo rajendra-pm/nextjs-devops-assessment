119d6499b124f621e13f85b275be1c2e45f58ea2
Rajendra P M <rajendrapm03@gmail.com>
2025-10-07 15:01:28 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Next.js App Deployed with Docker, GHCR & Minikube</h1>
+      <p>By Rajendra P M</p>
+    </div>
+  );
+}
